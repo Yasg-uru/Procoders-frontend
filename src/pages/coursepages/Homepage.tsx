53e3729c224d0b0dae4f128709b77202b8e78c2e
yasg-uru <yashpawar12122004@gmail.com>
2024-07-31 22:01:04 +0530

    created time ago functionality of notes

--- src/pages/coursepages/Homepage.tsx
+++ src/pages/coursepages/Homepage.tsx
@@ -221,7 +221,7 @@ import { Filtertype } from "@/types/CourseTypes/FilterTypes";
 import { ArrowRight, Loader2 } from "lucide-react";
 import React, { Fragment, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useMediaQuery } from "@uidotdev/usehooks";
+// import { useMediaQuery } from "@uidotdev/usehooks";
 
 const Homepage: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -230,7 +230,7 @@ const Homepage: React.FC = () => {
   const { categoryWiseCourses } = useAppSelector((state) => state.course);
   const [groupedCourses, setGroupedCourses] = useState<CategoryTypes>({});
   const { isAuthenticated } = useAppSelector((state) => state.auth);
-  const isMobile = useMediaQuery("(max-width: 600px)");
+  // const isMobile = useMediaQuery("(max-width: 600px)");
   const coursesRef = useRef<HTMLDivElement>(null);
 
   const handleClick = (category: string) => {
