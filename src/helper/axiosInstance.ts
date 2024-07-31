6b1d781a5fdec88c1e81a0e15c994db1b7988a0b
yasg-uru <yashpawar12122004@gmail.com>
2024-07-31 13:50:20 +0530

    created changes for deployment

--- src/helper/axiosInstance.ts
+++ src/helper/axiosInstance.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://advance-online-learning-platform.vercel.app",
 });
 export default axiosInstance;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,14 +5,14 @@ import { ThemeProvider } from "./components/theme-provider.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./redux/store.ts";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Provider store={store}>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <App />
-      <Toaster />
-    </ThemeProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <App />
+        <Toaster />
+      </ThemeProvider>
     </Provider>
   </BrowserRouter>
 );
