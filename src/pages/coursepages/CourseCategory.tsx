27d77d558dd8dca7d6ac98e0569dc0b70bb61be1
yasg-uru <yashpawar12122004@gmail.com>
2024-07-31 17:29:35 +0530

    created small ui change

--- src/pages/coursepages/CourseCategory.tsx
+++ src/pages/coursepages/CourseCategory.tsx
@@ -114,7 +114,7 @@ const CourseCategory: React.FC = () => {
   return (
     <div className="relative min-h-screen  justify-center bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200">
       <div className="flex flex-col items-center mb-6">
-        <h1 className="text-xl text-gray-900 dark:text-gray-100 mb-2">
+        <h1 className="text-xl text-gray-900 dark:text-gray-100 mb-2 my-3">
           Choose Your Program
         </h1>
         <hr className="w-full border-t-2 border-gray-300 dark:border-gray-700" />
